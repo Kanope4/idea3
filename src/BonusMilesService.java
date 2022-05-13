580d2f2b643135eabb7ca031e7063196410d0425
Anastasia N <nastka_kapito@mail.ru>
2022-05-13 14:57:14 +0300

    addedall

---
+++ src/BonusMilesService.java
@@ -0,0 +1,10 @@
+public class BonusMilesService {
+
+    public void calculate(int cost) {
+
+        int miles = cost / 20;
+
+        System.out.println("Бонусные мили" + miles);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BonusMilesService BonusMilesService = new BonusMilesService();
+
+        BonusMilesService.calculate(20000);
+    }
+}
